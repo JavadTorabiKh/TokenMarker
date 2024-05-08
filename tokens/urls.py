fdd70313a041dcd9ad610704548d4840931bc47c
javad <j.2528840@gmail.com>
2024-05-08 14:51:03 +0330

    v1

--- tokens/urls.py
+++ tokens/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('create_token/', views.create_token, name='create_token'),
     path('user/', views.UserList.as_view(), name='get_users'),
     path('user/<int:pk>/', views.UserDetail.as_view(), name='get_user'),
 
--- tokens/views.py
+++ tokens/views.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import status
 from .models import Token, User
 from rest_framework.response import Response
@@ -41,9 +42,17 @@ class TokenList(generics.ListCreateAPIView):
     serializer_class = TokenSerializer
     pagination_class = PaginationSize
 
-    # authentication
-    # authentication_classes = [JWTAuthentication]
-    # permission_classes = [IsAuthenticated]
+
+@api_view(["GET"])
+def create_token(request: Request):
+    res = requests.get("http://localhost:5000/")
+    data = res.json()
+
+    token_serializer = TokenSerializer(data=data["result"], many=True)
+    if token_serializer.is_valid():
+        token_serializer.save()
+        return Response({"status": "success", "data": token_serializer.data}, status.HTTP_201_CREATED)
+    return Response({"status": "erorrs"}, status.HTTP_400_BAD_REQUEST)
 
 
 class TokenDetail(generics.RetrieveDestroyAPIView):
